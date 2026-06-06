559d7f050937f3e6b0ac76e6d9795d472469d348
joeatethebeans <joewood.hav@gmail.com>
2026-06-07 02:50:18 +0900

    refactor(TermModification): changed parameter names to reflect model

--- Kyogo.Application/Persistence/ITermModificationRepository.cs
+++ Kyogo.Application/Persistence/ITermModificationRepository.cs
@@ -6,8 +6,8 @@ namespace Kyogo.Application.Persistence;
 
 public interface ITermModificationRepository
 {
-    public Task<TermModification?> GetAsync(UserId userId, TermId termId, CancellationToken cancellationToken = default);
-    public Task<IReadOnlyCollection<TermModification>> GetAllByUserAsync(UserId userId, CancellationToken cancellationToken = default);
+    public Task<TermModification?> GetAsync(UserId ownerId, TermId termId, CancellationToken cancellationToken = default);
+    public Task<IReadOnlyCollection<TermModification>> GetAllByOwnerAsync(UserId ownerId, CancellationToken cancellationToken = default);
     public Task AddAsync(TermModification modification,  CancellationToken cancellationToken = default);
     public Task RemoveAsync(TermModification modification, CancellationToken cancellationToken = default);
     public Task UpdateAsync(TermModification modification, CancellationToken cancellationToken = default);
