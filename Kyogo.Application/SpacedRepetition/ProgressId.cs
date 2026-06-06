09d7157cd26c4e4b485e867f4a669936de518cf8
joeatethebeans <joewood.hav@gmail.com>
2026-06-06 22:01:05 +0900

    refactor(ProgressId): restructured for parity with other Id objects

--- Kyogo.Application/SpacedRepetition/ProgressId.cs
+++ Kyogo.Application/SpacedRepetition/ProgressId.cs
@@ -2,17 +2,17 @@ using Kyogo.Domain.Vocabulary;
 
 namespace Kyogo.Application.SpacedRepetition;
 
-public readonly record struct ProgressId(string Id)
+public readonly record struct ProgressId(string Value)
 {
     public static ProgressId Create(TermId termId, CardType cardType)
         => new ProgressId($"{cardType}:{termId.Value}");
     
     public static TermId DeriveTermId(ProgressId progressId)
-        => new TermId(Guid.Parse(progressId.Id.Split(':')[1]));
+        => new TermId(Guid.Parse(progressId.Value.Split(':')[1]));
 
     public static CardType DeriveCardType(ProgressId progressId)
-        => Enum.TryParse(progressId.Id.Split(':')[0], true, out CardType cardType) ? cardType : throw new InvalidOperationException($"ProgressId {progressId} has an invalid card type");
+        => Enum.TryParse(progressId.Value.Split(':')[0], true, out CardType cardType) ? cardType : throw new InvalidOperationException($"ProgressId {progressId} has an invalid card type");
 
     public override string ToString() 
-        => Id;
+        => Value;
 }
