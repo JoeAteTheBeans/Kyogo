a2155bd039caae88c83f34814ad46b8c28756d16
joeatethebeans <joewood.hav@gmail.com>
2026-06-07 07:54:49 +0900

    fix(UserRepository): fixed method signature to reflect async

--- Kyogo.Application/Persistence/IUserRepository.cs
+++ Kyogo.Application/Persistence/IUserRepository.cs
@@ -4,7 +4,7 @@ namespace Kyogo.Application.Persistence;
 
 public interface IUserRepository
 {
-    public Task<User?> GetById(UserId id, CancellationToken cancellationToken = default);
+    public Task<User?> GetByIdAsync(UserId id, CancellationToken cancellationToken = default);
     public Task<User?> GetByUsernameAsync(string username, CancellationToken cancellationToken = default);
     public Task<User?> GetByEmailAsync(string email, CancellationToken cancellationToken = default);
     public Task AddAsync(User user, CancellationToken cancellationToken = default);
