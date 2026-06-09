6a21b885384692ea3abf1ed42baf5e9c0e06c730
joeatethebeans <joewood.hav@gmail.com>
2026-06-10 05:30:34 +0900

    fix(AuthenticationService): change method signatures to match async usage and add default cancellation tokens

--- Kyogo.Application/Authentication/AuthenticationService.cs
+++ Kyogo.Application/Authentication/AuthenticationService.cs
@@ -30,26 +30,26 @@ public sealed class AuthenticationService(IUserRepository userRepository, IToken
         return new RegisterResult(RegisterResultState.Success, tokenGeneratorService.GenerateToken(registering), refreshToken);
     }
 
-    public async Task<LoginResult> LoginWithUsernameAsync(string username, string password, CancellationToken cancellationToken)
+    public async Task<LoginResult> LoginWithUsernameAsync(string username, string password, CancellationToken cancellationToken = default)
     {
         User? user = await userRepository.GetByUsernameAsync(username, cancellationToken);
-        return user is null ? new LoginResult(LoginResultState.UsernameDoesNotExist, null, null) : await Login(user, password);
+        return user is null ? new LoginResult(LoginResultState.UsernameDoesNotExist, null, null) : await LoginAsync(user, password, cancellationToken);
     }
 
-    public async Task<LoginResult> LoginWithEmailAsync(string email, string password, CancellationToken cancellationToken)
+    public async Task<LoginResult> LoginWithEmailAsync(string email, string password, CancellationToken cancellationToken = default)
     {
         User? user = await userRepository.GetByEmailAsync(email, cancellationToken);
-        return user is null ? new LoginResult(LoginResultState.EmailDoesNotExist, null, null) : await Login(user, password);
+        return user is null ? new LoginResult(LoginResultState.EmailDoesNotExist, null, null) : await LoginAsync(user, password, cancellationToken);
     }
 
-    private async Task<LoginResult> Login(User user, string password)
+    private async Task<LoginResult> LoginAsync(User user, string password, CancellationToken cancellationToken = default)
     {
         if (!BCrypt.Net.BCrypt.EnhancedVerify(password, user.PasswordHash))
             return new LoginResult(LoginResultState.IncorrectPassword, null, null);
 
         RefreshToken refreshToken = tokenGeneratorService.GenerateRefreshToken(user);
-        await refreshTokenRepository.AddAsync(refreshToken);
-        await unitOfWork.SaveChangesAsync();
+        await refreshTokenRepository.AddAsync(refreshToken, cancellationToken);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
         return new LoginResult(LoginResultState.Success, tokenGeneratorService.GenerateToken(user), refreshToken);
     }
 }
